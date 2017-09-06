eb98f51672fd6b8c70972b5c9552733e21f6fadd
ilya shusterman <ilyashusterman@outlook.com>
2017-09-06 19:05:39 +0300

    added gitignore

--- hash.py
+++ hash.py
@@ -14,11 +14,14 @@ def main():
                         'ilyailyua'.encode(),
                         hashlib.sha256)
     key = hash_key.hexdigest()
-    print(hmac.compare_digest(key))
-    # print(json.dumps({'key': key}, indent=2))
+    # print(hmac.compare_digest(key))
+    print(json.dumps({'key': key}, indent=2))
     print(base64.b64decode(key, ))
     base64.b64encode(key.encode()).decode()
-
+    dig = hmac.new(b'1234567890', msg=your_bytes_string,
+                   digestmod=hashlib.sha256).digest()
+    base64.b64encode(dig).decode()  # py3k-mode
+    'Nace+U3Az4OhN7tISqgs1vdLBHBEijWcBeCqL5xN9xg='
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
