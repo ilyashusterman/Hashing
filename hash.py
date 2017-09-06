5b8858426f40e91236e1fa2330188bd9a378c0d9
ilya shusterman <ilyashusterman@outlook.com>
2017-09-06 23:56:50 +0300

    fixed encryption

--- hash.py
+++ hash.py
@@ -4,24 +4,64 @@ import hmac
 import json
 import logging
 import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+def encode(key, string):
+    encoded_chars = []
+    for i in range(len(string)):
+        key_c = key[i % len(key)]
+        encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = "".join(encoded_chars)
+    return base64.urlsafe_b64encode(encoded_string)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dry-run', help='Simulate, do not create AppNexus objects', action='store_true')
+    parser.add_argument('--dry-run',
+                        help='Simulate, do not create AppNexus objects',
+                        action='store_true')
     args = parser.parse_args()
+    encrypt_only()
+    # set password
+    password = 'mysecretpassword'
+    # set message
+    message = 'secretmessage'
+
+    encrypt = generate(message, password, salt='static_salt',
+                       action='encrypt')
+    print(encrypt.decode())
+    decrypt = generate(encrypt, password, salt='static_salt', action='decrypt')
+    print(decrypt.decode())
+
+
+def encrypt_only():
     hash_key = hmac.new('1234'.encode(),
                         'ilyailyua'.encode(),
                         hashlib.sha256)
     key = hash_key.hexdigest()
     # print(hmac.compare_digest(key))
     print(json.dumps({'key': key}, indent=2))
-    print(base64.b64decode(key, ))
-    base64.b64encode(key.encode()).decode()
-    dig = hmac.new(b'1234567890', msg=your_bytes_string,
-                   digestmod=hashlib.sha256).digest()
-    base64.b64encode(dig).decode()  # py3k-mode
-    'Nace+U3Az4OhN7tISqgs1vdLBHBEijWcBeCqL5xN9xg='
+
+
+def generate(message, password, salt, action):
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32,
+                     salt=salt.encode(),
+                     iterations=100000, backend=default_backend())
+    key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+    f = Fernet(key)
+
+    if action is 'encrypt':
+        encrypted = f.encrypt(message.encode())
+        return encrypted
+    elif action is 'decrypt':
+        decrypted = f.decrypt(message)
+        return decrypted
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
